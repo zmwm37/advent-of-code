9be6f12e4e9022bd25083ec4c06c2482c5c3d02d
Zander Meitus <zmeitus@uchicago.edu>
2023-12-16 09:28:17 -0700

    add day4 solution

--- 2023/day4.py
+++ 2023/day4.py
@@ -6,13 +6,13 @@ def parse_card(card:str):
     '''
     card = card.replace(":", "|")
     card_elements = card.split("|")
-    print(f'card elements {card_elements}')
+    #print(f'card elements {card_elements}')
     w = [int(i) for i in card_elements[1].split(' ') if i != '']
     H = [int(i) for i in card_elements[2].split(' ') if i != '']
 
     w.sort() # O(|w|)
-    print(f'w {w}')
-    print(f'H {H}')
+    #print(f'w {w}')
+    #print(f'H {H}')
     return (w, H)
 
 def search_winners(w:list, H:list):
@@ -23,8 +23,8 @@ def search_winners(w:list, H:list):
     p = 0
     q = len(w)
     c = 0
-    r = 0
-    for h in H:
+    r = 0 # TODO: remove
+    for i, h in enumerate(H):
         w = orig_w
         p = 0 
         q = len(orig_w)
@@ -34,7 +34,7 @@ def search_winners(w:list, H:list):
             idx = len(w) // 2
             comparison = w[idx]
             if h == comparison:
-                print(f'h {h} = comparison {comparison}!')
+                #print(f'h {h} = comparison {comparison}!')
                 c += 1
                 r = 0
                 break
@@ -61,20 +61,30 @@ def search_winners(w:list, H:list):
             q = len(w) - 1
     return c
 
-def count_cards(cards:list):
+def count_cards(cards:list)->tuple:
     '''
     Iterate through cards and get total points
     '''
     card_points = []
-    for card in cards:
+    card_counts = [1] * len(cards)
+    parent_cards = [ [] for _ in range(len(cards)) ]
+    total_card_count = 0
+    for i, card in enumerate(cards):
         w, H = parse_card(card)
         c = search_winners(w, H)
         if c > 0:
             card_points.append(2 ** (c - 1))
         else:
             card_points.append(0)
-    print(f'card points: {card_points}')
-    return sum(card_points)
+        for k in range(0, c):
+            parent_cards[i + k + 1].append(i)
+            card_counts[i + k + 1] += 1
+        card_count = 1
+        for parent in parent_cards[i]:
+            card_count += card_counts[parent]
+        card_counts[i] = card_count
+        total_card_count += card_count
+    return (sum(card_points), total_card_count)
 
 
 if __name__ == "__main__":
@@ -90,6 +100,7 @@ if __name__ == "__main__":
     with open(FILEPATH) as f:
         raw = f.readlines()
         lines = [i.strip('\n') for i in raw]
-        total_points = count_cards(lines)
+        total_points, total_card_count = count_cards(lines)
         print(f'TOTAL POINTS: {total_points}')
+        print(f'TOTAL CARDS: {total_card_count}')
 
